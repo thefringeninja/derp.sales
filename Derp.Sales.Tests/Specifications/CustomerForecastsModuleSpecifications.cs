ff933e1a0ea9664d37057ba740ba3a1769581e62
thefringeninja <joao.paul@braganca.name>
2013-11-07 13:57:04 -0800

    first passing test

---
+++ Derp.Sales.Tests/Specifications/CustomerForecastsModuleSpecifications.cs
@@ -0,0 +1,23 @@
+﻿using Derp.Sales.Tests.Fixtures;
+using Derp.Sales.Tests.Templates;
+using Derp.Sales.Web.Features.CustomerForecasts;
+using Nancy;
+using Simple.Testing.ClientFramework;
+
+namespace Derp.Sales.Tests.Specifications
+{
+    public class CustomerForecastsModuleSpecifications
+    {
+        public Specification viewing_list_of_customers()
+        {
+            return new ModuleSpecification<CustomerForecastsModule>
+            {
+                When = () => UserAgent.Get("/customer-forecasts"),
+                Expect =
+                {
+                    sut => sut.Response.StatusCode.Is(HttpStatusCode.OK)
+                }
+            };
+        }
+    }
+}
---
+++ Derp.Sales.Web/Features/CustomerForecasts/CustomerForecastsModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Derp.Sales.Web.Features.CustomerForecasts
+{
+    public class CustomerForecastsModule : NancyModule
+    {
+        public CustomerForecastsModule()
+            : base("/customer-forecasts")
+        {
+            Get["/"] = _ => 200;
+        }
+    }
+}
