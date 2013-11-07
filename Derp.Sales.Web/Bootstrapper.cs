633c53c3520e3926455bc189f742401415e17690
thefringeninja <joao.paul@braganca.name>
2013-11-07 13:19:57 -0800

    organizing by feature

---
+++ Derp.Sales.Web/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Conventions;
+
+namespace Derp.Sales.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            OrganizationByFeature.Enable(nancyConventions);
+        }
+    }
+}
---
+++ Derp.Sales.Web/OrganizationByFeature.cs
@@ -0,0 +1,24 @@
+﻿using Nancy.Conventions;
+
+namespace Derp.Sales.Web
+{
+    public static class OrganizationByFeature
+    {
+        public static void Enable(NancyConventions nancyConventions)
+        {
+            nancyConventions.ViewLocationConventions.Clear();
+
+            nancyConventions.ViewLocationConventions.Add(
+                (viewName, model, viewLocationContext) =>
+                    "features" + viewLocationContext.ModulePath.Underscore().Pascalize() + "/views/" + viewName);
+
+            nancyConventions.ViewLocationConventions.Add(
+                (viewName, model, viewLocationContext) =>
+                    "features/home/views/" + viewName);
+
+            nancyConventions.ViewLocationConventions.Add(
+                (viewName, model, viewLocationContext) =>
+                    "features/" + viewName);
+        }
+    }
+}
