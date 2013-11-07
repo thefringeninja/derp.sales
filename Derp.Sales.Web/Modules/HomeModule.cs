8f652130188c630fd9f9e35f9f9d010b325feaa0
thefringeninja <joao.paul@braganca.name>
2013-11-07 12:14:08 -0800

    Home view

---
+++ Derp.Sales.Web/Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿using Derp.Sales.Web.ViewModels;
+using Nancy;
+
+namespace Derp.Sales.Web.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => Negotiate.WithModel(new HomeViewModel());
+        }
+    }
+}
---
+++ Derp.Sales.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Derp.Sales.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+            Text = "Hello world";
+        }
+
+        public string Text { get; private set; }
+    }
+}
