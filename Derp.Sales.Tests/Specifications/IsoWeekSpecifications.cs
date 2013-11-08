947b90db286bc9548ac166e81fbad93b56e99593
thefringeninja <joao.paul@braganca.name>
2013-11-08 09:31:12 -0800

    iso week

---
+++ Derp.Sales.Tests/Specifications/IsoWeekSpecifications.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Derp.Sales.Domain;
+using Simple.Testing.ClientFramework;
+
+namespace Derp.Sales.Tests.Specifications
+{
+    public class IsoWeekSpecifications
+    {
+        public IEnumerable<Specification> iso_week()
+        {
+            var rules = new Dictionary<string, string>
+            {
+                {"1/1/2005", "2004-W53"},
+                {"1/2/2005", "2004-W53"},
+                {"12/31/2005", "2005-W52"},
+                {"1/1/2007", "2007-W01"},
+                {"12/30/2007", "2007-W52"},
+                {"12/31/2007", "2008-W01"},
+                {"1/1/2008", "2008-W01"},
+                {"12/28/2008", "2008-W52"},
+                {"12/29/2008", "2009-W01"},
+                {"12/30/2008", "2009-W01"},
+                {"12/31/2008", "2009-W01"},
+                {"1/1/2009", "2009-W01"},
+                {"12/31/2009", "2009-W53"},
+                {"1/1/2010", "2009-W53"},
+                {"1/2/2010", "2009-W53"},
+                {"1/3/2010", "2009-W53"},
+            };
+            var specifications = new List<Specification>();
+            foreach (var rule in rules)
+            {
+                var date = DateTime.Parse(rule.Key);
+                var theIsoWeek = rule.Value;
+                var name = "ISO Week of " + date.ToLongDateString();
+                specifications.Add(new QuerySpecification<DateTime, string>
+                                      {
+                                          Name = name,
+                                          On = () => date,
+                                          When = d => IsoWeek.FromDate(d),
+                                          Expect =
+                                          {
+                                              result => theIsoWeek == result
+                                          }
+                                      });
+            }
+            return specifications;
+        }
+    }
+}
---
+++ Derp.Sales/Domain/IsoWeek.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace Derp.Sales.Domain
+{
+    public class IsoWeek
+    {
+        private readonly int year;
+        private readonly int week;
+
+        private IsoWeek(int year, int week)
+        {
+            this.year = year;
+            this.week = week;
+        }
+
+        // This presumes that weeks start with Monday.
+        // Week 1 is the 1st week of the year with a Thursday in it.
+        private static Tuple<int, int> GetIso8601WeekOfYear(DateTime time)
+        {
+            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
+            // be the same week# as whatever Thursday, Friday or Saturday are,
+            // and we always get those right
+            var day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            
+            var year = time.Year;
+
+            var weekOfYear = CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            if (weekOfYear == 53 && (day > DayOfWeek.Thursday || day == DayOfWeek.Sunday))
+            {
+                year--;
+            }
+            return Tuple.Create(year, weekOfYear);
+        } 
+
+        public static IsoWeek FromDate(DateTime dateTime)
+        {
+            var week = GetIso8601WeekOfYear(dateTime);
+            return new IsoWeek(week.Item1, week.Item2);
+        }
+
+
+        public static implicit operator string(IsoWeek isoWeek)
+        {
+            return isoWeek.ToString();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0:D4}-W{1:D2}", year, week);
+        }
+    }
+}
